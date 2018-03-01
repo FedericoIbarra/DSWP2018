28625d502b2d03584a07832845df767f0823d577
FedericoIbarra <is702981@iteso.mx>
2018-02-28 18:45:17 -0600

    Colores asi bien tremendos

--- app/src/main/java/mx/iteso/sportsquare/ActivityMain.java
+++ app/src/main/java/mx/iteso/sportsquare/ActivityMain.java
@@ -1,7 +1,9 @@
 package mx.iteso.sportsquare;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
 
