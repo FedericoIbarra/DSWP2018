ede8443dd1b0a2abd1cdd593909a8dc3503208f8
FedericoIbarra <is702981@iteso.mx>
2018-03-09 18:30:07 -0600

    Hash Class

---
+++ app/src/main/java/mx/iteso/sportsquare/Hash.java
@@ -0,0 +1,39 @@
+package mx.iteso.sportsquare;
+
+/**
+ * Created by federicoibarra on 09/03/18.
+ */
+
+public class Hash {
+
+    /**
+     * @return un String con el Hash del texto.
+     * @param txt el texto a encriptar.
+     * @param hashType el tipo de encriptacion.
+     */
+
+    private static String getHash(String txt, String hashType) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance(hashType);
+            byte[] array = md.digest(txt.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+                        .substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     *  @return un hash SHA1 a partir de un texto
+     *  */
+    public static String sha1(String txt) {
+        return Hash.getHash(txt, "SHA1");
+    }
+
+}
