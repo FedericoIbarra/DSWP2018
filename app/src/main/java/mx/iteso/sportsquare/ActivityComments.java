f6faa389bbd7f499ffc1b54d5e3940ac1f5c59b1
Elias <eGasparArellano@gmail.com>
2018-05-01 19:01:36 -0500

    ActivityComments

---
+++ app/src/main/java/mx/iteso/sportsquare/ActivityComments.java
@@ -0,0 +1,13 @@
+package mx.iteso.sportsquare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityComments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
